7871df336c7eb08c4b59e55a22bef934c65c0fe2
b4dPot4to <eduard@swarm.com>
2026-06-23 15:48:32 +0200

    remove firebase

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['192.168.1.101'],
-  output: 'export',
   reactStrictMode: true,
-  images: {
-    unoptimized: true
-  }
 };
 
 export default withNextIntl(nextConfig);
